cf67d01b185bff5d04b165d2becc3c61423a0c69
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2022-11-14 21:43:30 -0300

    Translate and refactor

---
+++ js/translate.js
@@ -0,0 +1,9 @@
+function redirectPage(termo) {
+    if (termo == "en") {
+        window.location.replace(
+            "https://jonatasfelipe.github.io/index_en.html"
+        );
+    } else if (termo == "pt") {
+        window.location.replace("https://jonatasfelipe.github.io");
+    }
+}
